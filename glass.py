060b828085625f5cde9bed9a0def94090b5ba68d
Jonathan Coles <jonathan@jpcoles.com>
2009-05-15 11:58:15 +0000

    Added pseudo-command model() to initiate model generation from input file.
    git-svn-id: https://svn.physik.uzh.ch/repos/itp/glass@8 13158d06-3fb6-11de-ada5-f312106159a4

--- glass.py
+++ glass.py
@@ -6,17 +6,15 @@ def help():
     print >>sys.stderr, "Usage: glass.py <input>"
     sys.exit(2)
 
-if __name__ == "__main__":
-
-    if len(sys.argv) < 2: help()
-    from commands import *
-    execfile(sys.argv[1])
-
+# Although this is technically a command, we need it here so that it
+# can see 'init_model_generator' which will be defined by the executed
+# input file.
+def model(nmodels):
     for o in env.objects:
         print o.name
         o.init()
 
-    prepare_for_models(env.objects)
+    init_model_generator()
 
     if 0:
         model = empty(2002)
@@ -27,10 +25,11 @@ if __name__ == "__main__":
     #print arriv_grid(model, env.objects[0])
     #print mass_grid(model, env.objects[0])
 
-    #for i,[sol,m] in enumerate(generate_models(env.objects, 1)): print i
-
+    env.models = []
+    for i,m in enumerate(generate_models(env.objects, nmodels)): 
+        env.models.append(m)
     #mass_plot([env.objects[0], {'mass':None}])
-    potential_plot([env.objects[0], {'mass':None}])
+    #potential_plot([env.objects[0], {'mass':None}])
     #mass_plot(m[0])
 
     #arrival_plot(m[0][0], m[0][1])
@@ -48,4 +47,10 @@ if __name__ == "__main__":
             #for v in sol: print >>f, v
             #print >>f
         f.close()
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2: help()
+    from commands import *
+    execfile(sys.argv[1])
 
