16edd8c1001b0cfb454feec31311d0624b3c3e61
Jonathan Coles <jonathan@jpcoles.com>
2009-06-14 12:07:09 +0000

    Added steep() command to replace minsteep/maxsteep pair. Added clear() to reset the environment if the input file loops over multiple configurations. Added delay() to added extra time delays between non-consective arrival times. Moved subdiv() to the commands from the pixel basis. Renamed lens() to source() for clarity.
    git-svn-id: https://svn.physik.uzh.ch/repos/itp/glass@47 13158d06-3fb6-11de-ada5-f312106159a4

--- glcmds.py
+++ glcmds.py
@@ -4,20 +4,19 @@ from numpy import arctan2
 from environment import env, Image, System
 from shear import Shear
 import cosmo
+from handythread import parallel_map
 
 def ptmass(xc, yc, mmin, mmax): assert False, "ptmass not implemented"
 def redshifts(*args):           assert False, "redshifts not implemented"
-def double(*args):              assert False, "double() not supported. Use lens()."
-def quad(*args):                assert False, "quad() not supported. Use lens()."
-def multi(*args):               assert False, "multi() not supported. Use lens()."
+def double(*args):              assert False, "double() not supported. Use source()."
+def quad(*args):                assert False, "quad() not supported. Use source()."
+def multi(*args):               assert False, "multi() not supported. Use source()."
+def minsteep(a):                assert False, "minsteep not supported. Use steepness()."
+def maxsteep(a):                assert False, "maxsteep not supported. Use steepness()."
+
 
 def globject(name):
-    env.new_object(name)
-
-def subdiv(n):
-    n = int(n)
-    assert (n > 2 and n%2==0), "subdiv: n must be odd and > 2"
-    env.current_object().S = s
+    return env.new_object(name)
 
 def shear(phi):
     env.current_object().shear = Shear(phi)
@@ -35,34 +34,44 @@ def cosm(om, ol):
 #    print 'lens() is now deprecated. Use source() instead.'
 #    source(zsrc, img0, img0parity, imgs)
 
-def lens(zsrc, img0, img0parity, *imgs):
+def source(zsrc, img0=None, img0parity=None, *imgs):
 
     o = env.current_object()
+
+    assert o.zlens is not None, "zlens() must first be specified."
     assert zsrc >= o.zlens, "Source is not behind lens."
 
     # XXX: this changes the scales for each lens with a different zsrc!!!
     #o.zlens, o.tscale, o.tscalebg, o.dlscale, o.cdscale = cosmo.scales(o.zlens,zsrc)
-    sys = System(cosmo.angdist(0,zsrc) / cosmo.angdist(o.zlens,zsrc))
+    sys = System(zsrc, o.zlens)
 
-    image0 = Image(img0, img0parity)
-    sys.add_image(image0)
+    if img0 is not None and img0parity is not None:
+        image0 = Image(img0, img0parity)
+        sys.add_image(image0)
 
-    prev = image0
-    for i in xrange(0, len(imgs), 3):
-        img,parity,time_delay = imgs[i:i+3]
-        if prev.parity_name == 'sad' and parity == 'max':
-            prev.angle = arctan2(prev.pos.imag-img[1], 
-                                 prev.pos.real-img[0]) * 180/math.pi
-        image = Image(img, parity)
-        sys.add_image(image)
-        sys.add_time_delay(prev,image, time_delay)
-        prev = image
+        prev = image0
+        for i in xrange(0, len(imgs), 3):
+            img,parity,time_delay = imgs[i:i+3]
+            if prev.parity_name == 'sad' and parity == 'max':
+                prev.angle = arctan2(prev.pos.imag-img[1], 
+                                     prev.pos.real-img[0]) * 180/math.pi
+            image = Image(img, parity)
+            sys.add_image(image)
+            sys.add_time_delay(prev,image, time_delay)
+            prev = image
 
     o.add_system(sys)
+    return sys
 
+def delay(A,B, delay):
+    """ Add a time delay between images A and B such that B arrive delay days after A. """
+    sys = env.current_object().current_system()
+    a = sys.images[sys.images.index(A)]
+    b = sys.images[sys.images.index(B)]
+    sys.add_time_delay(a,b,delay)
 
-def symm(v=True):
-    """Turn lens symmetry on or off. Default is on."""
+def symm(v=False):
+    """Turn lens symmetry on or off. Default is off."""
     assert False, "Symmetry not yet supported."
     env.current_object().symm = v
 
@@ -70,11 +79,9 @@ def dgcone(theta):
     assert (0 < theta <= 90), "dgcone: need 0 < theta <= 90"
     env.current_object().cen_ang = (90-theta) * math.pi/180
 
-def minsteep(a):
-    env.current_object().minsteep = a
-
-def maxsteep(a):
-    env.current_object().maxsteep = a
+def steep(lb, ub):
+    env.current_object().minsteep = lb
+    env.current_object().maxsteep = ub
 
 def g(h):
     env.h_spec = 1.0/h
@@ -85,12 +92,15 @@ def kann(theta):
 def maprad(r):
     env.current_object().maprad = r
 
+def clear():
+    env.clear()
 
 def postfilter(*fs):
     models = env.models
     if fs:
         for m in models: m['tagged'] = True
         for f in fs:     models = filter(f, models)
+        #for f in fs:     models = filter(parallel_map(f, models, threads=env.ncpus))
         for m in models: m['tagged'] = False
 
     env.accepted_models = models
