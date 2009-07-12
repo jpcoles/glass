d918e6be3b0b8fd922153fd1378c4bc27fcb67ba
Jonathan Coles <jonathan@jpcoles.com>
2009-07-12 19:05:44 +0000

    New scaling functions with better names.
    git-svn-id: https://svn.physik.uzh.ch/repos/itp/glass@72 13158d06-3fb6-11de-ada5-f312106159a4

--- scales.py
+++ scales.py
@@ -15,6 +15,29 @@ def density_to_physical(*args):
     else:              obj,v,H0inv    = args
     return v * obj.scales['critden'] * H0inv
 
+def MsunKpc2_to_MsunArcsec2(*args):
+    pass
+
+def MsunKpc2_to_KappaKpc2(*args):
+    pass
+
+def MsunKpc2_to_KappaArcsec2(*args):
+    if len(args) == 2: [obj, data], v = args; H0inv = data['1/H0']
+    else:              obj,v,H0inv    = args
+    return v * (obj.scales['angdist'] * H0inv)**2 \
+             / (obj.scales['critden'] * H0inv)
+
+def KappaArcsec2_to_MsunKpc2(*args):
+    if len(args) == 2: [obj, data], v = args; H0inv = data['1/H0']
+    else:              obj,v,H0inv    = args
+    return v / (obj.scales['angdist'] * H0inv)**2 \
+             * (obj.scales['critden'] * H0inv)
+
+def KappaArcsec2_to_MsunArcsec2(*args):
+    if len(args) == 2: [obj, data], v = args; H0inv = data['1/H0']
+    else:              obj,v,H0inv    = args
+    return v * (obj.scales['critden'] * H0inv)
+
 #-------------------------------------------------------------------------------
 # Distance conversions
 #-------------------------------------------------------------------------------
@@ -29,6 +52,16 @@ def distance_to_physical(*args):
     else:              obj,v,H0inv    = args
     return v * obj.scales['angdist'] * H0inv
 
+def Kpc_to_Arcsec(*args):
+    if len(args) == 2: [obj, data], v = args; H0inv = data['1/H0']
+    else:              obj,v,H0inv    = args
+    return v / obj.scales['angdist'] / H0inv
+
+def Arcsec_to_Kpc(*args):
+    if len(args) == 2: [obj, data], v = args; H0inv = data['1/H0']
+    else:              obj,v,H0inv    = args
+    return v * obj.scales['angdist'] * H0inv
+
 #-------------------------------------------------------------------------------
 # Time conversions
 #-------------------------------------------------------------------------------
@@ -37,5 +70,11 @@ def time_to_physical(obj, v):
     return 1 / v / obj.scales['time']
 
 def time_to_internal(obj, v):
+    return 1 / v / obj.scales['time']
+
+def Days_to_Expansion(obj, v):
+    return 1 / v / obj.scales['time']
+
+def Expansion_to_Days(obj, v):
     return 1 / v / obj.scales['time']
 
