Switch all functions to use 'kappa' instead of 'kappa DM'. This produced incorrect results if stellar mass was also present.
git-svn-id: https://svn.physik.uzh.ch/repos/itp/glass@211 13158d06-3fb6-11de-ada5-f312106159a4

glass/basis/pixels/funcs.py
@@ -70,7 +70,7 @@ def arrival_time(m):
 
             tau  = abs(theta-src)**2 / 2
             tau *= s.zcap
-            tau -= dot(ps['kappa DM'], poten(theta - obj.basis.ploc, obj.basis.cell_size, obj.basis.maprad))
+            tau -= dot(ps['kappa'], poten(theta - obj.basis.ploc, obj.basis.cell_size, obj.basis.maprad))
             tau -= np.sum( [ ps[e.name] * e.poten(theta).T for e in obj.extra_potentials ] )
 
             taus.append(tau)