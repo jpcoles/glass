cb1f58713d8f390aa0b6ef89dd7a7abb418108bc
Rafael Kueng <rafik@physik.uzh.ch>
2015-07-08 13:08:04 +0200

    added command line execution to ensem.py

--- postproc/ensem.py
+++ postproc/ensem.py
@@ -1,10 +1,38 @@
-mname = 'ASW0007k4r/012771'
-mname = 'ASW0000h2m/007022'
-mname = 'ASW0000h2m/gribbles'
-mname = 'WM4H5RZXQZ'
-#mname = 'IHRULOMX6D'
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+"""
+modified p sahas ensem to work from command line with args
 
-state = loadstate(mname+'.state')
+Created on Wed Jul  8 12:57:52 2015
+@author: psaha, rafik
+"""
+
+import sys
+import os.path
+import pickle
+
+
+try: # read first parameter from command line (remember: 0 is the script file name)
+    mname = sys.argv[1]
+
+except IndexError: # otherwise use hardcoded filename
+    mname = 'ASW0007k4r/012771'
+    mname = 'ASW0000h2m/007022'
+    mname = 'ASW0000h2m/gribbles'
+    mname = 'WM4H5RZXQZ'
+    #mname = 'IHRULOMX6D'
+    mname = 'WM4H5RZXQZ'
+    
+
+    mname = mname + '.state'
+
+#check if the file really exists
+if not os.path.isfile(mname):
+    print "no valid file! (%s)" % mname
+    sys.exit('no valid file')
+
+
+state = loadstate(mname)
 
 grids = []
 for m in state.models:
@@ -14,7 +42,7 @@ for m in state.models:
     
 imgradii = [abs(img.pos) for img in state.objects[0].sources[0].images]
 
-
+# create dict to be pickled afterwards
 d = {
     'grids': grids,
     'maprad': obj.basis.maprad,
@@ -22,8 +50,14 @@ d = {
     'maximgpos': max(imgradii),
 }
     
-import pickle
 
 with open(mname+'.pkl','w') as fil:
     pickle.dump(d,fil)
+
+
+
+
+
+
+
 
