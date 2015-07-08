c91a53c581d872db98d6e7993d3ca76878e4fb67
Rafael Kueng <rafik@physik.uzh.ch>
2015-07-08 12:26:16 +0200

    added maximgpos

--- postproc/ensem.py
+++ postproc/ensem.py
@@ -2,7 +2,7 @@ mname = 'ASW0007k4r/012771'
 mname = 'ASW0000h2m/007022'
 mname = 'ASW0000h2m/gribbles'
 mname = 'WM4H5RZXQZ'
-mname = 'IHRULOMX6D'
+#mname = 'IHRULOMX6D'
 
 state = loadstate(mname+'.state')
 
@@ -11,13 +11,16 @@ for m in state.models:
     obj,data = m['obj,data'][0]
     g = obj.basis._to_grid(data['kappa DM'])
     grids.append(g)
+    
+imgradii = [abs(img.pos) for img in state.objects[0].sources[0].images]
 
 
 d = {
     'grids': grids,
     'maprad': obj.basis.maprad,
     'pixrad': obj.basis.pixrad,
-    }
+    'maximgpos': max(imgradii),
+}
     
 import pickle
 
