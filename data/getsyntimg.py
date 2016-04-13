f1a93f15a5ab3e3673a5b84b33a35ea59e70923d
Rafael Kueng <rafi.kueng@gmx.ch>
2016-04-13 19:33:58 +0200

    clean up and fixes for new synth img

--- data/getsyntimg.py
+++ data/getsyntimg.py
@@ -15,7 +15,41 @@ import matplotlib.pyplot as plt
 import imgproc as ip
 
 
-#from pprint import pprint
+# load and cut the image to roi
+
+img_name = '7k4r.png'
+
+# start with 3 hardcoded squares
+shapes = [
+    ('sq', (48,73,146,161)),
+    ('sq', (18,28,176,191)),
+    ('sq', (60,71,184,196)),
+]
+
+# this only works for squares, it's a shortcut for the paper plot below.. don't acutally use this for production
+# shapes2 = [ (t,c, np.s_[c[0]:c[1],c[2]:c[3]]) for t,c in shapes ]
+
+# select region / cut
+# (x1,x2,y1,y2)
+# x is vertical
+cut = (10,80,140,200)
+cut = (0,100,120,220)
+
+cutslice = np.s_[cut[0]:cut[1],cut[2]:cut[3]] # can directly be used on images
+
+# setup the center / lens pos.. (in reference to the NEW / cutted image)
+centrx = 177.95
+centry = -53.54
+# set offsets
+offsx = cut[2]
+offsy = cut[0]
+
+center = (centrx-offsx, centry-offsy) 
+
+
+image = ip.get_image(img_name, shapes, cut)
+image2 = np.copy(image)
+
 
 
 glass_basis('glass.basis.pixels', solver=None)
@@ -52,14 +86,14 @@ def delta_beta(theta):
 
 pixel = []
 
-for x, xrow in enumerate(ip.image):
+for x, xrow in enumerate(image):
     for y, rgb in enumerate(xrow):
         if sum(rgb) != 0:           #TODO make this depend on a binary mask
             pixel.append((x,y))
 
 #pix_arr = np.array(pixel)
 
-theta = map(ip.px2arcs, pixel)
+theta = [ip.px2arcs(p, center) for p in pixel]
 d_source = np.array(map(delta_beta, theta))
 #print pix_arr
 
@@ -101,7 +135,9 @@ for i, pnt in enumerate(d_source):
 
 
 #nimage = np.clip(ip.image+255,0,255)
-nimage = 0*ip.image
+
+image = np.copy(image2)
+nimage = 0 * image
 
 for (x,y), lst in np.ndenumerate(pxllist):
     n = len(lst)    
@@ -109,7 +145,7 @@ for (x,y), lst in np.ndenumerate(pxllist):
         summ = np.array([0,0,0], dtype=np.int)
         for i in lst:
             ix,iy = pixel[i]
-            summ += ip.image[ix,iy]
+            summ += image[ix,iy]
         summ = np.uint8(summ / n)
         #print summ.dtype
         
@@ -125,14 +161,49 @@ for (x,y), lst in np.ndenumerate(pxllist):
 #            if x==M and y==M:
 #                nimage[ix,iy] = np.array([255,0,0], dtype=np.uint8)
 
-diff = np.array(ip.image, dtype=np.int16)
-diff = np.array(np.clip(np.abs(diff - nimage)*2,0,255), dtype=np.uint8)
 
-f, (ax0, ax1, ax2, ax3) = plt.subplots(1, 4)
+if False:
+    diff = np.array(image, dtype=np.int16)
+    diff = np.array(np.clip(np.abs(diff - nimage),0,255), dtype=np.int16)
+    diff = np.array((diff[:,:,0]+diff[:,:,1]+diff[:,:,2]) / 3. , dtype=np.uint8)
+    
+    f, (ax0, ax1, ax2, ax3) = plt.subplots(1, 4)
+    
+    ax0.imshow(ip.img, interpolation='none')
+    ax1.imshow(image, interpolation='none')
+    ax2.imshow(nimage, interpolation='none')
+    ax3.imshow(diff, interpolation='none', cmap="gray")
+    plt.show()
 
-ax0.imshow(ip.img, interpolation='none')
-ax1.imshow(ip.image, interpolation='none')
-ax2.imshow(nimage, interpolation='none')
-ax3.imshow(diff, interpolation='none')
-plt.show()
+
+# nice plot for paper
+f, axs = plt.subplots(1, 2)
+(ax0, ax1) = axs
+
+inpimg = np.copy(ip.img)
+
+drawcol = np.array([0,255,0], dtype=np.uint8)
+
+for t,c in shapes:
+    x1,x2,y1,y2 = c
+    for x in range(x1-1,x2+2):
+        inpimg[x,y1-1,:] = drawcol
+        inpimg[x,y2+1,:] = drawcol
+    for y in range(y1-1,y2+2):
+        inpimg[x1-1,y,:] = drawcol
+        inpimg[x2+1,y,:] = drawcol
+
+inpimg = inpimg[cutslice]
+
+ax0.imshow(inpimg, interpolation='none')
+ax1.imshow(nimage, interpolation='none')
+
+for ax in axs:
+    ax.axis("off")
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+#f.set_size_inches(2, 1)
+plt.savefig('new_synth_img.png', bbox_inches='tight', pad_inches = 0, dpi=300)
+#plt.show()
 
--- data/imgproc.py
+++ data/imgproc.py
@@ -7,37 +7,59 @@ import matplotlib.pyplot as plt
 
 
 
-def px2arcs(coords):
+def px2arcs(coords, center, pxsize = 0.187):
 
     x = coords[1]
     y = -coords[0]
 
+    cx, cy = center
 #    return x+1j*y
-
-    centrx = 177.95
-    offsx = 100.0
-    centry = -53.54
-    offsy = 0.0
-
-    pxsize = 0.187
     
-    x = ( x-(centrx-offsx) ) * pxsize
-    y = ( y-(centry-offsy) ) * pxsize
+#    x = ( x-(centrx-offsx) ) * pxsize
+#    y = ( y-(centry-offsy) ) * pxsize
+
+    x = ( x-cx ) * pxsize
+    y = ( y-cy ) * pxsize
     
     return x+1j*y
     
 
+img = None
+image = None
 
+def get_image(iname, shapes, cut):
 
-img = misc.imread('7k4r.png')
-nimg = np.zeros(img.shape, dtype=np.uint8)
-
-nimg[48:73,146:161,:] = img[48:73,146:161,:]
-nimg[18:28,176:191,:] = img[18:28,176:191,:]
-nimg[60:71,184:196,:] = img[60:71,184:196,:]
-
-image = nimg[0:100,100:200,:]
-
+    global img
+    global image
+    
+    img = misc.imread(iname)
+    nimg = np.zeros(img.shape, dtype=np.uint8)
+    
+#    # start with 3 hardcoded squares
+#    shapes = [
+#        ('sq', (48,73,146,161)),
+#        ('sq', (18,28,176,191)),
+#        ('sq', (60,71,184,196)),
+#    ]
+#    
+#    # select region / cut
+#    # (x1,x2,y1,y2)
+#    # x is vertical
+#    cut = (0,100,100,200)
+#    
+    
+    for tjpe, parameters in shapes:
+        if tjpe=="sq": #square:
+            x1,x2,y1,y2 = parameters
+            nimg[x1:x2,y1:y2,:] = img[x1:x2,y1:y2,:]
+    
+    #nimg[48:73,146:161,:] = img[48:73,146:161,:]
+    #nimg[18:28,176:191,:] = img[18:28,176:191,:]
+    #nimg[60:71,184:196,:] = img[60:71,184:196,:]
+    
+    image = nimg[cut[0]:cut[1], cut[2]:cut[3], : ]
+    
+    return image
 
 #bimg = np.zeros(img.shape[0:1], dtype=np.bool)
 #
